348beee27dfdb28247e7aabe4eda6d1252cf1ae7
Atsushi Nagaki <atsushi-nagaki@cryptogarage.co.jp>
2024-02-13 21:17:21 +0900

    change max duration to 5s

--- src/pages/test6.tsx
+++ src/pages/test6.tsx
@@ -7,14 +7,14 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 export default function Index({ text }: Props) {
   return (
     <>
-      <h1>Test5</h1>
+      <h1>Test6</h1>
       <p>{text}</p>
     </>
   )
 }
 
 export const getServerSideProps = async () => {
-  await sleep(5000)
+  await sleep(6000)
   return {
     props: { text: "Hello World!" },
   }
